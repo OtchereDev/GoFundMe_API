2ab5eb1158542c46502e48152707ec727f779bd9
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-11 07:04:30 +0000

    add func for creating new fundraiser

--- src/fundraiser/entity/fundraiser.repo.ts
+++ src/fundraiser/entity/fundraiser.repo.ts
@@ -1,8 +1,25 @@
+import { User } from 'src/user/entity/user.entity'
 import { Repository, EntityRepository } from 'typeorm'
+import { FundraiserDTO } from '../dto/fundraiser.dto'
+import { Category } from './category.entity'
 import { Fundraiser } from './fundraiser.entity'
 
 
 @EntityRepository(Fundraiser)
 export class FundraiserRepository extends Repository<Fundraiser>{
+
+    async createFundraiser(body:FundraiserDTO,organiser:User):Promise<Fundraiser>{
+
+        let category=await Category.findOne({name:body.category})
+
+        if (!category){
+            category=await Category.create({name:body.category}).save()
+        }
+        const fundraiserData={...body,organiser,category:[category],image_url:"/google/"}
+
+        return this.create({...fundraiserData}).save()
+
+
+    }
 
 }
