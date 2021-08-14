378a6850dde237f8d4ddfc487b4b5f401c6df09c
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-14 08:59:44 +0000

    integrated stripe as gateway

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import {  Module,} from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Donation } from './entity/donation.entity';
 import { DonationRepository } from './entity/donation.repo';
+import { PaymentIntent } from './entity/payment-intent.entity';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 
+
 @Module({
+  imports:[TypeOrmModule.forFeature([
+    Donation,DonationRepository,PaymentIntent
+  ])],
   controllers: [PaymentsController],
-  providers: [PaymentsService, DonationRepository]
+  providers: [PaymentsService]
 })
-export class PaymentsModule {}
+export class PaymentsModule {
+ 
+}
