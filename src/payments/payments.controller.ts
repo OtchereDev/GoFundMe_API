d432a22361e03e9f9a8e1dd30eca29896d17e76b
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-14 08:57:23 +0000

    added routes for intent generation and webhook for stripe

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe,Headers, Req, Param } from '@nestjs/common';
+import { Charge } from './dto/charge.dto';
+import { CommentDTO } from './dto/comment.dto';
 import { PaymentsService } from './payments.service';
 
 @Controller('payments')
@@ -6,13 +8,30 @@ export class PaymentsController {
 
     constructor(private paymentService:PaymentsService){}
 
-    @Post()
-    handlePayment(){
-
+    @Post('/create-payment')
+    @UsePipes(ValidationPipe)
+    async handlePayment(@Body() body:Charge){
+        return await this.paymentService.createPaymentIntent(
+                                        body.amount,
+                                        body.fundraiser_id,
+                                        body.name
+                                        )
+        
     }
 
-    @Post()
-    handleWebhook(){
 
+    @Post('/:intent_id/add-comment')
+    @UsePipes(ValidationPipe)
+    async addPayerComment(@Body() body:CommentDTO, 
+                            @Param('intent_id') intent_id:string){
+
+        return await this.paymentService.addPayerComment(intent_id,body.message)
+    }
+
+    @Post('/webhook')
+    async handleWebhook(@Body() body,@Headers('stripe-signature') sign:string,@Req() req){
+        
+        await this.paymentService.handleWebhook(body,sign,req)
+        return 
     }
 }
