0ced43177fbca3a0863417452b3e7a19abc99abc
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-15 23:55:51 +0000

    added swagger to fundraiser dto

--- src/fundraiser/dto/fundraiser.dto.ts
+++ src/fundraiser/dto/fundraiser.dto.ts
@@ -1,29 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsAlpha, IsNotEmpty, Length, Max, Min } from 'class-validator'
 
 export class FundraiserDTO{
 
     @IsNotEmpty()
     @IsAlpha()
+    @ApiProperty()
     category:string
 
     @IsNotEmpty()
     @Length(5,150)
+    @ApiProperty()
     title:string
 
     @IsNotEmpty()
+    @ApiProperty()
     description:string
 
     @IsNotEmpty()
     @Length(2,100)
     @IsAlpha()
+    @ApiProperty()
     beneficiary:string
 
     @IsNotEmpty()
     @Min(100)
     @Max(1000000)
+    @ApiProperty()
     goal_amount:number
 
     @IsNotEmpty()
     @IsAlpha()
+    @ApiProperty()
     country:string
 }
