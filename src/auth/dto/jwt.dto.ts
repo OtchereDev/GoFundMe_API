a9c0d2f009b9e2db335c3fd3f2e805819cbedd9a
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-15 23:53:55 +0000

    added swagger def for jwt dto

--- src/auth/dto/jwt.dto.ts
+++ src/auth/dto/jwt.dto.ts
@@ -1,9 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger'
 import {IsJWT, IsNotEmpty} from 'class-validator'
 
 export class JwtDTO {
 
     @IsNotEmpty()
     @IsJWT()
+    @ApiProperty()
     refresh_token:string
     
     
