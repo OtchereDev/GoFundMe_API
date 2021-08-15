66f0881978c6b12bd20a300e9a4d2e2f2660de71
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-15 21:44:18 +0000

    added swagger to application

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import {NestExpressApplication} from '@nestjs/platform-express'
 import { join } from 'path';
 import * as express from 'express'
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 
 
 async function bootstrap() {
@@ -16,6 +17,18 @@ async function bootstrap() {
     origin:'*'
   })
   app.use( express.json({verify:(req:any,res,buf)=>{req.rawBody=buf}}));
+
+  const config = new DocumentBuilder()
+                    .setTitle("GoFundMe API")
+                    .setDescription("This is the API definition of the Clone of GoFundMe")
+                    .setVersion("1.0")
+                    .setContact("Oliver Otchere","you.tube/c/OtchereDev","Oliverotchere4@gmail.com")
+                    .build()
+
+  const document = SwaggerModule.createDocument(app,config)
+
+  SwaggerModule.setup("docs",app,document)
+
   await app.listen(3000);
 }
 bootstrap();
