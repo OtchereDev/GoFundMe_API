cfbb994601c5226ca8acac4e69eceabf72ecd4cc
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-15 23:53:31 +0000

    added swaggerdef for auth dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger'
 import {IsEmail, IsNotEmpty, } from 'class-validator'
 
 export class AuthDTO{
 
     @IsEmail()
     @IsNotEmpty()
+    @ApiProperty()
     email:string
 
     @IsNotEmpty()
+    @ApiProperty()
     password:string
 }
