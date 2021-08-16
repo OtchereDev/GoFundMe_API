8c71381a681b6bc6354f863c2b02922009d96674
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-16 00:05:54 +0000

    change type to class and added swagger def

--- src/user/types/UserSerializer.type.ts
+++ src/user/types/UserSerializer.type.ts
@@ -1,5 +1,14 @@
-export type  UserCreateSerializer={
-    email:string,
-   
+import { ApiProperty } from "@nestjs/swagger"
+
+export class  UserCreateSerializer{
+    @ApiProperty({
+        default:'johndoe@hmail.com'
+    })
+    email:string
+
+
+    @ApiProperty({
+        default:1
+    })
     id:number
 }
