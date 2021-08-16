724d5c9d10030d2e0ec26a50eb200019916afdff
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-16 00:06:55 +0000

    added swagger touser dto

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,14 +1,21 @@
 import { IsEmail, IsNotEmpty,MinLength,Matches } from "class-validator"
+import { ApiProperty } from "@nestjs/swagger"
 
 export class UserDTO{
 
     @IsNotEmpty({
         message:'please provide a Full Name'
     })
+    @ApiProperty({
+        description:'full user name'
+    })
     fullName:string
 
     @IsEmail({},{
         message:'please provide a valid email'
+    })
+    @ApiProperty({
+        description:"User email"
     })
     email:string
 
@@ -17,6 +24,9 @@ export class UserDTO{
     })
     @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&^_-]{8,}$/,{
         message:'please provide a strong password'
+    })
+    @ApiProperty({
+        description:'User Strong Password',
     })
     password:string
 
