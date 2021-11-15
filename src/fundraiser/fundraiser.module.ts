e559c84da609285f5db954e94f6322a001282a78
Oliver Otchere <ootchere@payswitch.com.gh>
2021-11-15 16:50:42 +0000

    added user module

--- src/fundraiser/fundraiser.module.ts
+++ src/fundraiser/fundraiser.module.ts
@@ -1,18 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryRepository } from 'src/category/models/category.repo';
+import { UserModule } from 'src/user/user.module';
 import { Category } from './entity/category.entity';
 import { Fundraiser } from './entity/fundraiser.entity';
 import { FundraiserRepository } from './entity/fundraiser.repo';
 import { FundraiserController } from './fundraiser.controller';
 import { FundraiserService } from './fundraiser.service';
+import { FundraiserResolver } from './resolver/fundraiser.resolver';
 
 @Module({
   imports:[TypeOrmModule.forFeature([
-    Fundraiser,Category,FundraiserRepository,   
-  ]),
+    Fundraiser,Category,FundraiserRepository,CategoryRepository  
+  ]),UserModule ,
   ],
   controllers: [FundraiserController],
-  providers: [FundraiserService,]
+  providers: [FundraiserService,FundraiserResolver],
+  exports:[
+    TypeOrmModule
+  ]
 })
 export class FundraiserModule {}
