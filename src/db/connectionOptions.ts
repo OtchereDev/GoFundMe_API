cfccb42cf6ab5b1b95b40715de6649cdb2fd17d1
Oliver Otchere <oliverotchere4@gmail.com>
2021-11-18 10:42:55 +0000

    added postgres config

--- src/db/connectionOptions.ts
+++ src/db/connectionOptions.ts
@@ -1,8 +1,15 @@
-import {SqliteConnectionOptions} from 'typeorm/driver/sqlite/SqliteConnectionOptions'
+// import {SqliteConnectionOptions} from 'typeorm/driver/sqlite/SqliteConnectionOptions'
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
+// export const option:SqliteConnectionOptions={
+//     type:'sqlite',
+//     database:'db.sqlite3',
+//     entities:["dist/**/*.entity{.ts,.js}"],
+//     synchronize:true
+// }
 
-export const option:SqliteConnectionOptions={
-    type:'sqlite',
-    database:'db.sqlite3',
+export const option:PostgresConnectionOptions={
+    type:"postgres",
+    url: process.env.DATABASE_URL,
+    synchronize:true,
     entities:["dist/**/*.entity{.ts,.js}"],
-    synchronize:true
 }
